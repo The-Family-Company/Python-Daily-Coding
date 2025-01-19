606982494bbaf5a192e405de27381f19d7ba0962
aysha26rin <ayshanasrin2606@gmail.com>
2025-01-19 20:27:22 +0530

    aysha-grade.py

--- grade.py
+++ grade.py
@@ -1,11 +1,14 @@
 mark=int(input("Enter a number"))
-
-if mark>= 90:
+if mark >= 90 and mark <= 100:
     print("A grade")
-elif mark>= 70:
+elif mark >= 80 and mark <=89:
     print("B grade")
-elif mark>= 35 :
+elif mark >= 70 and mark <=79:
     print("C grade")
+elif mark >= 60 and mark <=69:
+    print("D grade")
+elif mark >= 50 and mark <=59:
+    print("E grade")
+
 else:
-    mark!=35
     print("fail")
