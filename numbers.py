bddf6e18215463ee2641dea2a1a483fc85204251
aysha26rin <ayshanasrin2606@gmail.com>
2025-01-20 19:37:40 +0530

    aysha-number-sign

---
+++ numbers.py
@@ -0,0 +1,7 @@
+number=int(input("Enter a number:"))
+if number>0:
+    print("This is the positive number")
+elif number<0:
+    print("This is the negative number")
+else:
+    print("This is the zero")
