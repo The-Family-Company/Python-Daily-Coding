70e2cbb5242eeb2b48a82eb10743ca7a07d92368
aysha26rin <ayshanasrin2606@gmail.com>
2025-01-22 21:37:48 +0530

    loops-an-python

--- forloop.py
+++ forloop.py
@@ -1,3 +1,4 @@
+#for
 anime=["naruto","sakura","itachi","kakashi","sasuke","gaara"]
 for x in anime:
   if x=="sakura":
@@ -5,4 +6,12 @@ for x in anime:
   print(x)
   if x=="sasuke":
    break
-  
+#while
+i=0
+while i < 9:
+ i += 1
+ if i == 3:
+  continue
+ print(i)
+ #do while
+ 
