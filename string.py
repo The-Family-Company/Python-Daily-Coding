49379572c7f50bf8dbe71645c77a40da22e61383
aysha26rin <ayshanasrin2606@gmail.com>
2025-01-23 20:20:47 +0530

    aysha-string

---
+++ string.py
@@ -0,0 +1,25 @@
+#string is immutable
+name="aysha"
+name.title()
+print("String cannot be changed:",name)
+#you can change str to convert
+#first letter will be capital
+Name=name.title()
+print("First letter convert into capital:",Name)
+#slicing to the str
+me="Aysha Nasrin"
+nam=len(me)
+print("Length of str:",nam)
+my_name="Sha Rin"
+My_name=my_name[0:3]
+print("Slicing of the str:",My_name)
+#simple prgm of str function method
+person="aysha"
+pet_name="alice"
+pet_type="cat"
+print(person,"has a",pet_type,"and his name is",pet_name)
+print(f"{person} has a {pet_type} and his name is {pet_name}")
+print("{}".format(person), "has a {}".format(pet_type), "and his name is {}".format(pet_name) )
+#thanks
+thanks="see you tomorrow,bye"
+print("Tomorrow i would try in str methods",thanks)
