08f6bede17fc9f765c82bb346f89c4465e3c0ed8
aysha26rin <ayshanasrin2606@gmail.com>
2025-01-24 20:58:29 +0530

    nested if.py

--- if.py
+++ if.py
@@ -1,19 +1,19 @@
 #nested if
 num=int(input("Enter a Number:"))
-if (num>50)and(num<80):
+if (num>20)and(num<100):
     print("This Block Is Executed Successfully")
     if(num==60)or(num==100):
         print("This block Is Almost successfully Correct")
     else:
         print("Second block is not  correct!")
 #sample task in nested if
-        print("Voting section,welcome")
 name=(input("Enter your name:"))
+print("welcome to voting section",name)
 age=int(input("Enter your age:"))
 rule=(input("do you have a voter id:"))
 if age>18:
     print("you can eligible for voting!")
-    if rule=="yes"and rule=="no":
+    if (rule=="yes")or(rule=="Yes")or(rule=="YES"):
         print("you go to voting section")
     else:
         print("if you can eligible!but you don't have voter id")
