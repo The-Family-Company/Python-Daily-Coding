4f288d471acd61e3042cb7208d9114f597040cd1
aysha26rin <ayshanasrin2606@gmail.com>
2025-01-24 20:13:11 +0530

    aysha-str-method

---
+++ string-method.py
@@ -0,0 +1,11 @@
+#string method/function
+str="Aysha Nasrin"
+print(str.split())
+print(str.lower())
+print(str.capitalize())
+print(str.upper())
+print(str.find("h"))
+print(str.strip("Nasrin"))
+print(str.endswith("n"))
+print(str.startswith("H"))
+print(str.replace("Nasrin","Hawwa"))
