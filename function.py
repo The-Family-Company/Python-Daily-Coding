9b2850de90d4977b927a893cde29194d26fa185d
hawwa26 <hawwanoorin26@gmail.com>
2025-01-26 20:00:04 +0530

    hawwa-function

--- function.py
+++ function.py
@@ -5,7 +5,7 @@ def get_smaller_numbers (numbers, n):
             smaller_numbers.append(number)
     return smaller_numbers
 
-numbers_list = [10, 25, 8, 15, 3, 40, 21, 26, 23, 100]
+numbers_list = [10, 25, 8, 15, 3, 40,56,67,78]
 n = 55
 result = get_smaller_numbers(numbers_list, n)
-print("Smaller Numbers:",n,result)
+print("Numbers smaller than:", n,result)
